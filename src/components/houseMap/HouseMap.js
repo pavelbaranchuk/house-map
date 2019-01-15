7a510a5487966a3427457b45cb7511112ac5667a
Pavel Baranchuk <pbaranchuk@gmail.com>
2019-01-15 14:28:07 +0200

    Small fix

--- src/components/houseMap/HouseMap.js
+++ src/components/houseMap/HouseMap.js
@@ -1,21 +1,21 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-import { withStyles } from "@material-ui/core/styles";
-import { Grid, Card, CardContent } from "@material-ui/core";
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import { Grid, Card, CardContent } from '@material-ui/core';
 
-import { connect } from "react-redux";
-import { loadTemplate } from "../../store/actionCreators/templates";
-import { loadHousesData } from "../../store/actionCreators/houses";
+import { connect } from 'react-redux';
+import { loadTemplate } from '../../store/actionCreators/templates';
+import { loadHousesData } from '../../store/actionCreators/houses';
 
-import Area from "../area/Area";
-import Image from "../image/Image";
-import Price from "../price/Price";
-import Address from "../address/Address";
+import Area from '../area/Area';
+import Image from '../image/Image';
+import Price from '../price/Price';
+import Address from '../address/Address';
 
 const styles = {
   houseWrapper: {
-    marginTop: "5rem"
-  }
+    marginTop: '5rem',
+  },
 };
 
 const templateN = 0;
@@ -27,7 +27,7 @@ class HouseMap extends Component {
     templates: PropTypes.object.isRequired,
     houses: PropTypes.object.isRequired,
     loadTemplate: PropTypes.func,
-    loadHousesData: PropTypes.func
+    loadHousesData: PropTypes.func,
   };
 
   componentDidMount() {
@@ -52,19 +52,19 @@ class HouseMap extends Component {
                   }
                   return (
                     <div key={i}>
-                      {item.component === "PRICE" && (
+                      {item.component === 'PRICE' && (
                         <Price
                           price={houses.response.data[houseN][item.field]}
                         />
                       )}
 
-                      {item.component === "ADDRESS" && (
+                      {item.component === 'ADDRESS' && (
                         <Address
                           address={houses.response.data[houseN][item.field]}
                         />
                       )}
 
-                      {item.component === "IMAGE" && (
+                      {item.component === 'IMAGE' && (
                         <Image
                           src={houses.response.data[houseN][item.field][0]}
                           insider={
@@ -73,7 +73,7 @@ class HouseMap extends Component {
                                 child.map((subitem, k) => {
                                   return (
                                     <div key={k}>
-                                      {subitem.component === "ADDRESS" && (
+                                      {subitem.component === 'ADDRESS' && (
                                         <Address
                                           address={
                                             houses.response.data[houseN][
@@ -83,7 +83,7 @@ class HouseMap extends Component {
                                         />
                                       )}
 
-                                      {subitem.component === "PRICE" && (
+                                      {subitem.component === 'PRICE' && (
                                         <Price
                                           inner
                                           price={
@@ -94,7 +94,7 @@ class HouseMap extends Component {
                                         />
                                       )}
 
-                                      {subitem.component === "AREA" && (
+                                      {subitem.component === 'AREA' && (
                                         <Area
                                           area={
                                             houses.response.data[houseN][
@@ -111,7 +111,7 @@ class HouseMap extends Component {
                         />
                       )}
 
-                      {item.component === "AREA" && (
+                      {item.component === 'AREA' && (
                         <Area area={houses.response.data[houseN][item.field]} />
                       )}
                     </div>
@@ -128,16 +128,16 @@ class HouseMap extends Component {
 const mapStateToProps = state => {
   return {
     templates: state.templates,
-    houses: state.houses
+    houses: state.houses,
   };
 };
 
 const mapDispatchToProps = {
   loadTemplate,
-  loadHousesData
+  loadHousesData,
 };
 
 export default connect(
   mapStateToProps,
-  mapDispatchToProps
+  mapDispatchToProps,
 )(withStyles(styles)(HouseMap));
