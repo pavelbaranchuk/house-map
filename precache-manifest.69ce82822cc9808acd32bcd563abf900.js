9860abe3d84f7b0e6482b649d69d228f8f9f450b
Pavel Baranchuk <pbaranchuk@gmail.com>
2019-01-16 19:06:57 +0200

    Create page for GH-Pages

---
+++ precache-manifest.69ce82822cc9808acd32bcd563abf900.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "1bc8ccec6a00ac7b3a94",
+    "url": "/house-map/static/js/runtime~main.1bc8ccec.js"
+  },
+  {
+    "revision": "e7e3e2b97e6e91c47b3b",
+    "url": "/house-map/static/js/main.e7e3e2b9.chunk.js"
+  },
+  {
+    "revision": "c1a807ea6193f92cd397",
+    "url": "/house-map/static/js/1.c1a807ea.chunk.js"
+  },
+  {
+    "revision": "f5655aad51f1886e72ebe7e26e652990",
+    "url": "/house-map/index.html"
+  }
+];
--- src/components/App.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import HouseMap from "./house/HouseMap";
-
-const App = () => <HouseMap />;
-
-export default App;
